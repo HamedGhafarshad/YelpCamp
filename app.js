7ba6f76c7a77736c8256830f072663ccd1c475a6
HamedGhafarshad <hamedghafarshad@gmail.com>
2017-08-07 13:35:17 -0700

    Added user sign up form through register get and post route

--- app.js
+++ app.js
@@ -15,16 +15,16 @@ var uristring = "mongodb://hghafars:trunks123@ds163612.mlab.com:63612/yelp_camp"
 
 //Connecting to our database, will create db if none exist
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
 
-//
-//mongoose.connect(uristring, function(err, res){
-//    if(err){
-//        console.log("error");
-//    }else{
-//        console.log("Success");
-//    }
-//});
+
+mongoose.connect(uristring, function(err, res){
+    if(err){
+        console.log("error");
+    }else{
+        console.log("Success");
+    }
+});
 
 
 // Passport Configuration
